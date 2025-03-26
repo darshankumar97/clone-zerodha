ef0d67f7775519d551bb0c73e628d6ae4d61f701
DARSHAN-KUMAR-KR <143259511+darshankumar97@users.noreply.github.com>
2025-03-26 23:10:00 +0530

    installed router and updated the index.js file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{BrowserRouter, Routes,Route } from "react-router-dom";
 import './index.css';
-import Homepage from './land_space/home/homepage';
+import homepage from './land_space/home/homepage';
+import signup from './land_space/signup/signup';
+import about from './land_space/about/aboutpage';
+import product from './land_space/product/productpage';
+import pricing from './land_space/pricing/pricingpage';
+import support from './land_space/support/supportpage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   
-    <Homepage/>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<homepage />}/>
+      <Route path="/signup" element={<signup />}/>
+      <Route path="/about" element={<aboutpage />}/>
+      <Route path="/product" element={<productpage />}/>
+      <Route path="/pricing" element={<pricingpage />}/>
+      <Route path="/support" element={<supportpage />}/>
+      </Routes>
+  </BrowserRouter>
   
-  </React.StrictMode>
 );
 
 
--- src/land_space/footer.js
+++ src/land_space/footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 function Footer () {
     return ( 
-        <div className='container'>
-            <div className='row'>
+        <footer style={{backgroundColor:"rgb(250,250,250"}}>
+        <div className='container border-top mt-5'>
+            <div className='row mt-4'>
                 <div className='col'>
                     <img src='media/logo.svg' style={{width:'50%'}}></img>
                     <p>© 2010 - 2025, Zerodha Broking Ltd.
@@ -40,7 +41,39 @@ All rights reserved.</p>
                     
                 </div>
             </div>
+            
+            <div className="mt-5 fs-6 text-muted" style={{fontSize:"14px"}}>
+        
+
+<p class='mt-10'>Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to <a href="">complaints@zerodha.com</a>, for DP related to <a href="">dp@zerodha.com</a>. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF</p> <br></br>
+
+<p>Procedure to file a complaint on <a href="">SEBI SCORES</a>: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances</p> <br></br>
+
+<a href="">Smart Online Dispute Resolution | Grievances Redressal Mechanism</a><br></br>
+
+<p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
+
+<p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p><br></br>
+
+<p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, <a href="">please create a ticket here</a>.</p>
+</div>
+
+
         </div>
+        <div className="container mt-3 space-between">
+        <a href="" className="mr-2">NSE</a>
+        <a href="">BSE</a>
+        <a href="">MCX</a>
+        <a href="">Terms & Condition</a>
+        <a href="">Policies & Procedure</a>
+        <a href="">Privacy policy</a>
+        <a href="">Disclosure</a>
+        <a href="">For investor's attention</a>
+        <a href="">investor charter</a>
+
+        </div>
+        </footer>
+
      );
 }
 
